36137bb23f82529b91fe84d9ebdc1ce30c528ceb
jhooser <jhooser@gmail.com>
2019-11-20 20:57:18 -0600

    formatting functions

---
+++ formatjh.py
@@ -0,0 +1,9 @@
+def format(a = "-"):
+    print(str(a) * 50)
+
+def autoformat(string,a="-"):
+    print(str(a)*50)
+    print(f'{string}')
+    print(str(a)*50)
+
+autoformat("Gotta get that boom, boom, BOOM!!")
--- hello.py
+++
@@ -1,6 +0,0 @@
-def format(a)
-    print("a" * 50)
-
-print("Hello world")
-format(-)
-print(My name is John)
